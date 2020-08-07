8b681762f62e0a6a04ed7e40f26a5b38d12bd8d0
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-08-07 19:37:11 -0400

    Finished PTUI working with hangman and wins and losses

--- src/com/company/Hangman.java
+++ src/com/company/Hangman.java
@@ -26,10 +26,156 @@ public class Hangman {
             }
         }
         String solution = phrases.get((int)(Math.random()*407));
-        System.out.println(solution);
+        //System.out.println(solution);
+        solution = solution.toLowerCase();
+
+        PTUI(solution);
         //System.out.println((int)(Math.random()*407));
 
 
+    }
+
+    public static void PTUI(String solution){
+        int count = 1;
+        int letters_left = 0;
+        ArrayList<Boolean> rev = new ArrayList<>();
+        for(int i = 0;i<solution.length();i++){
+            if(solution.charAt(i) == ' '){
+                rev.add(true);
+            }else if(solution.charAt(i) == ',') {
+                rev.add(true);
+            }else if(solution.charAt(i) == '\''||(solution.charAt(i) == '.')||(solution.charAt(i) == '-')){
+                rev.add(true);
+            }else{
+                letters_left++;
+                rev.add(false);
+            }
+        }
+        Scanner sc = new Scanner((System.in));
+        //boolean done = false;
+
+        while(letters_left>0 && count !=7){
+
+            hang_words(solution,rev);
+            System.out.println();
+            System.out.println("Guess a letter");
+            String letterStr = sc.next();
+            char letter = letterStr.toCharArray()[0];
+            if(solution.contains(letterStr)){
+                for(int i = 0;i<solution.length();i++){
+                    if(solution.charAt(i)==letter){
+                        rev.set(i,true);
+                        letters_left--;
+
+                    }
+                }
+
+            }else{
+                count++;
+                System.out.println("Wrong guess, try again");
+            }
+            hangmanImage(count, solution);
+            System.out.println();
+        }
+        if(letters_left==0){
+            System.out.println("CONGRATS YOU WON");
+        }
+
+
+
+    }
+    public static void hang_words(String solution, ArrayList<Boolean> rev){
+        for(int i =0; i< solution.length();i++){
+            if(rev.get(i)){
+                System.out.print(solution.charAt(i));
+            }else{
+                System.out.print("_");
+            }
+            System.out.print(" ");
+        }
+    }
+    public static void hangmanImage(int count, String solution) {
+        if (count == 1) {
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("___|___");
+            System.out.println();
+        }
+        if (count == 2) {
+           // System.out.println("Wrong guess, try again");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (count == 3) {
+          //  System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   | ");
+            System.out.println("___|___");
+        }
+        if (count == 4) {
+          //  System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (count == 5) {
+          //  System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (count == 6) {
+       //     System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+        }
+        if (count == 7) {
+            System.out.println("GAME OVER!");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |          _|_");
+            System.out.println("   |         / | \\");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+            System.out.println("GAME OVER! The word was " + solution);
+        }
     }
 
 
