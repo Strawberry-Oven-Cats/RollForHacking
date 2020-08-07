146846808af5808418d50ef8572d475ae16f9876
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-08-07 16:35:03 -0400

    Issues with text file but getting hangman setup

--- src/com/company/Hangman.java
+++ src/com/company/Hangman.java
@@ -1,4 +1,35 @@
 package com.company;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Hangman {
+
+    //first making the ptui
+    public static void main() throws IOException {
+        File file =
+                new File("/Users/allasophy/Documents/GitHub/RollForHacking/Hangman_words.txt");
+        Scanner sc = new Scanner(file);
+
+        FileWriter myWriter = new FileWriter("Hangman_words.txt");
+        String line;
+
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            if (line.length()!=1) {
+                line = line.split("\\[")[0];
+                myWriter.write(line + "\n");
+                System.out.println(line);
+            }
+
+
+
+        }
+
+    }
 }
+
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("STRAWBERRY");
         System.out.println("Sydale");
+        Hangman.main();
     }
 }
