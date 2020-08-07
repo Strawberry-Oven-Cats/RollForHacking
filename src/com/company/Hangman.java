8080e792c97a28b15a496ce841f61a4fcff9c0d7
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-08-07 18:54:59 -0400

    Hangman class now generates a phrase from the hangman_words.txt. In additon all "[" were removed

--- src/com/company/Hangman.java
+++ src/com/company/Hangman.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hangman {
@@ -11,23 +12,27 @@ public class Hangman {
                 new File("/Users/allasophy/Documents/GitHub/RollForHacking/Hangman_words.txt");
         Scanner sc = new Scanner(file);
 
-        FileWriter myWriter = new FileWriter("Hangman_words.txt");
+        //FileWriter myWriter = new FileWriter("Hangman_words.txt");
         String line;
-
+        ArrayList<String> phrases = new ArrayList<>();
 
         while (sc.hasNextLine()) {
             line = sc.nextLine();
-            if (line.length()!=1) {
-                line = line.split("\\[")[0];
-                myWriter.write(line + "\n");
-                System.out.println(line);
+            if (line.length() != 1) {
+                //line = line.split("\\[")[0];
+                //myWriter.write(line + "\n")
+                phrases.add(line);
+                //System.out.println(line);
             }
-
-
-
         }
+        String solution = phrases.get((int)(Math.random()*407));
+        System.out.println(solution);
+        //System.out.println((int)(Math.random()*407));
+
 
     }
+
+
 }
 
 
