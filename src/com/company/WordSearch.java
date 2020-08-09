624bedff150bb82e625a66d8b81423df138ceb3c
srper <srp4581@rit.edu>
2020-08-08 21:13:26 -0400

    added solving functionality to WS

--- src/com/company/WordSearch.java
+++ src/com/company/WordSearch.java
@@ -38,6 +38,8 @@ public class WordSearch {
             wordBank.add(word);
         }
 
+        sc.close();
+
         // create the list of 10 words to be used in the grid from the word bank
         int i = 0;
         int index;
@@ -60,7 +62,7 @@ public class WordSearch {
         for (int i = 0; i < searchList.size(); i++) {
             String curWord = searchList.get(i);
             int curWordLength = curWord.length();
-            System.out.println("word: " + curWord + " length: " + curWordLength);
+            // System.out.println("word: " + curWord + " length: " + curWordLength);
             if ( i % 2 == 0) { // if its even, try to add it horizontally
                 // generate random coordinates until you find a valid pair
                 boolean isValid = false;
@@ -70,7 +72,7 @@ public class WordSearch {
                 while (!isValid) {
                     xcoor = rand.nextInt(rows);
                     ycoor = rand.nextInt(cols);
-                    System.out.println("coordinates x: " + xcoor + " y: " + ycoor);
+                    // System.out.println("coordinates x: " + xcoor + " y: " + ycoor);
 
                     // check that x coordinate + length of word < rows AND that we don't overwrite any other words
                     if (xcoor + curWordLength <= rows && checkCoordinateValidity(xcoor, ycoor, curWordLength, true)) {
@@ -95,7 +97,7 @@ public class WordSearch {
                 while (!isValid) {
                     xcoor = rand.nextInt(rows);
                     ycoor = rand.nextInt(cols);
-                    System.out.println("coordinates x: " + xcoor + " y: " + ycoor);
+                    // System.out.println("coordinates x: " + xcoor + " y: " + ycoor);
 
                     // check that y coordinate + length of word < cols AND that we don't overwrite any other words
                     if (ycoor + curWordLength <= cols && checkCoordinateValidity(xcoor, ycoor, curWordLength, false)) {
@@ -146,7 +148,7 @@ public class WordSearch {
     public void printGrid() {
         // prints the grid
         // currently only prints dummy data - underscores
-        System.out.print("   ");
+        System.out.print("    ");
         for (int x = 0; x < rows; x++) {
             System.out.print(x + " ");
         }
@@ -167,6 +169,70 @@ public class WordSearch {
         }
     }
 
+    public void solve() {
+        Scanner sc = new Scanner(System.in);
+        int wordsFound = 0;
+
+        while (wordsFound < 5) {
+            // take in user guesses
+            System.out.println("Guess the coordinates of a word:");
+            System.out.print("x start: ");
+            int xstart = sc.nextInt();
+            System.out.print("y start: ");
+            int ystart = sc.nextInt();
+            System.out.print("x end: ");
+            int xend = sc.nextInt();
+            System.out.print("y end: ");
+            int yend = sc.nextInt();
+
+            System.out.println("You entered start:" + xstart + ", " + ystart + " end:" + xend + ", " + yend);
+
+            // check that they are within bounds
+            if ( xstart < 0 || ystart < 0 || xend > rows || yend > cols) {
+                System.out.println("Invalid input! Try again.");
+                break;
+            } else {
+                // check if a valid word was found
+                if (checkForWord(xstart, ystart, xend, yend)) {
+                    wordsFound++;
+                }
+            }
+        }
+
+        System.out.println("All words found!");
+    }
+
+    public boolean checkForWord( int xstart, int ystart, int xend, int yend) {
+        StringBuilder sb = new StringBuilder();
+        if (xstart == xend) { // check vertically, change only the y's
+            for (int i = ystart; i <= yend; i++) {
+                sb.append(grid[xstart][i]);
+            }
+        } else if (ystart == yend) { // check horizontally, change only the x's
+            for (int i = xstart; i <= xend; i++) {
+                sb.append(grid[i][ystart]);
+            }
+        }
+        String guess = sb.toString();
+
+        // compare the guessed words to all the possible words in the search list
+        for (String word : searchList) {
+            if (word.equals(guess)) {
+                System.out.println("Word found: " + guess);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean main() throws FileNotFoundException{
+        WordSearch ws = new WordSearch();
+        System.out.println("Look for: " + ws.searchList);
+        ws.printGrid();
+        ws.solve();
+        return true;
+    }
+
     public static void main(String args[]) throws FileNotFoundException {
         // create an instance of WordSearch
         // start the game and a timer
@@ -174,5 +240,6 @@ public class WordSearch {
         WordSearch ws = new WordSearch();
         System.out.println("Look for: " + ws.searchList);
         ws.printGrid();
+        ws.solve();
     }
 }
